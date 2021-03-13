3cd96e89b15f1ce634ef21224aa8cda0066b773d
KitePig <mofapangshu@gmail.com>
2021-03-13 17:49:57 +0800

    await

--- algorithms/705.设计哈希集合/test2.py
+++ algorithms/705.设计哈希集合/test2.py
@@ -1,30 +1,38 @@
 class MyHashSet:
 
     def __init__(self):
-        self.buckets = 1009
-        self.table = [[] for _ in range(self.buckets)]
+        """
+        Initialize your data structure here.
+        """
+        self.bucket = 1000
+        self.set = [[]] * self.bucket # 创建定长拉链数组
 
     def hash(self, key):
-        return key % self.buckets
+        return key % self.bucket
 
-    def add(self, key):
-        hashkey = self.hash(key)
-        if key in self.table[hashkey]:
+    def add(self, key: int) -> None:
+        hash_key = self.hash(key)
+        if key in self.set[hash_key]:
             return
-        self.table[hashkey].append(key)
+        self.set[hash_key].append(key)
 
-    def remove(self, key):
-        hashkey = self.hash(key)
-        if key not in self.table[hashkey]:
+    def remove(self, key: int) -> None:
+        hash_key = self.hash(key)
+        if key not in self.set[hash_key]:
             return
-        self.table[hashkey].remove(key)
+        self.set[hash_key].remove(key)
 
-    def contains(self, key):
-        hashkey = self.hash(key)
-        return key in self.table[hashkey]
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        hash_key = self.hash(key)
+        return key in self.set[hash_key]
+
+
 
 # Your MyHashSet object will be instantiated and called as such:
-key = 120001
+key = 222
 obj = MyHashSet()
 obj.add(key)
 obj.remove(key)
