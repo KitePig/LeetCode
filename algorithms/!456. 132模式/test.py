73ee17fac2c8db20ffd436ceb4a9bee2ab81c4be
KitePig <mofapangshu@gmail.com>
2021-03-24 12:08:59 +0800

    await

---
+++ algorithms/!456. 132模式/test.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        N = len(nums)
+        leftMin = [float("inf")] * N
+        for i in range(1, N):
+            leftMin[i] = min(leftMin[i - 1], nums[i - 1])
+
+        stack = []
+        for j in range(N - 1, -1, -1):
+            numsk = float("-inf")
+            while stack and stack[-1] < nums[j]:
+                numsk = stack.pop()
+            if leftMin[j] < numsk:
+                return True
+            stack.append(nums[j])
+        return False
+
+obj = Solution()
+re = obj.find132pattern([3,1,4,2])
+print(re)
