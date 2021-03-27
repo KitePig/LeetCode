be7b1485e1be86d6f6c35664207532f44a3a1a0b
KitePig <mofapangshu@gmail.com>
2021-03-27 18:49:57 +0800

    learned

---
+++ algorithms/61. 旋转链表/test.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if k == 0 or not head or not head.next:
+            return head
+
+        n = 1
+        cur = head
+        while cur.next:
+            n += 1
+            cur = cur.next
+
+        if (add := n - (k % n)) == n:
+            return head
+
+        cur.next = head
+        while add:
+            cur = cur.next
+            add -= 1
+
+        ret = cur.next
+        cur.next = None
+        return ret
